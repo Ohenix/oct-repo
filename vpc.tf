42380a858dab89295b88b6cc99c6f0f36965163a
Julius <oheeneeba@icloud.com>
2022-11-17 17:43:27 +0000

    new modified

--- vpc.tf
+++ vpc.tf
@@ -1,10 +1,21 @@
 
+
+
+
 provider "aws" {
   region = "eu-west-2"
 }
 
 # creating VPC
 
-resource "aws_vpc" "July-class" {
+resource "aws_vpc" "october-class" {
   cidr_block = "10.0.0.0/16"
+  instance_tenancy = "default"
+  enable_dns_hostnames = true
+
+  tags = {
+    name = "october-class"
+  }
+
+  
 }
