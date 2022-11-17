43c9b7f84f1cb21e9a4da10223a70c964d18e4b9
Julius <oheeneeba@icloud.com>
2022-11-17 20:55:45 +0000

    updated vpc

--- vpc.tf
+++ vpc.tf
@@ -8,14 +8,12 @@ provider "aws" {
 
 # creating VPC
 
-resource "aws_vpc" "october-class" {
+resource "aws_vpc" "Great" {
   cidr_block = "10.0.0.0/16"
   instance_tenancy = "default"
   enable_dns_hostnames = true
 
   tags = {
-    name = "october-class"
+    name = "Great"
   }
-
-  
 }
